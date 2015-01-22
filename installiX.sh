b768d6fedfbdff4b4f68a79c68ac16a9daf3cd13
Andrew Baba <andrzej.gda@gmail.com>
2015-01-22 19:18:19 +0000

    Concatenated 2 scripts in 1

--- installiX.sh
+++ installiX.sh
@@ -1,10 +1,4 @@
-!/bin/bash
-
-## Things needed to do manually after script installation
-#  1.Enable Hardware Acceleration for Chrome/Chromium:
-#	In Chromium/chrome by default hardware acceleration is disabled for GPU, It can be enabled for better playback performance like videos, 3d stuff, etc.
-#	Type this address "chrome://flags" (without quotes) and locate "Override software rendering list" so you will find it disabled, so now enable it and Relaunch browser.
-#	After restart of browser type this address "chrome://gpu" (without quotes) and now check Hardware Acceleration will be enabled. See following screenshot.
+#!/bin/bash
 
 #install chromium
 sudo apt-get install chromium-browser
@@ -15,13 +9,12 @@ sudo apt-get update
 sudo apt-get install deepin-terminal
 
 #install newer flash player peppermint
-
 sudo add-apt-repository ppa:skunk/pepper-flash -y
 sudo apt-get update
 sudo apt-get install pepflashplugin-installer
 echo '. /usr/lib/pepflashplugin-installer/pepflashplayer.sh' | sudo tee -a /etc/chromium-browser/default
 
-#codecs
+#movie codecs
 sudo apt-get install ubuntu-restricted-extras
 
 #dropbox
@@ -30,27 +23,24 @@ sudo apt-get install dropbox python-gpgme
 #skype
 sudo apt-get install skype
 
-#information about your hardware 
+#information about your hardware
 sudo apt-get install hardinfo
-
-#install better fonts, change them manually with this http://www.binarytides.com/optimize-fonts-linux-mint/
-sudo apt-get install fonts-droid fonts-noto
-
 
 #install jdk java 8 from oracle
 sudo apt-get remove openjdk*
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
+
 #if you want accept oracle licence use line below
 #sudo apt-get install oracle-java8-installer
 #auto-aceptance of oracle terms
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+
 #automatically set up the Java 8 environment variables
 sudo apt-get install oracle-java8-set-default
 
-#unity tweak tool for system modification 
+#unity tweak tool for system modification
 sudo apt-get install unity-tweak-tool
-
 
 #guake - terminal
 sudo apt-get install guake
@@ -87,28 +77,34 @@ sudo add-apt-repository ppa:diesch/testing -y
 sudo apt-get update
 sudo apt-get install classicmenu-indicator
 
-
 #radio indicator
 sudo add-apt-repository ppa:eugenesan/ppa -y
-sudo apt-get update 
+sudo apt-get update
 sudo apt-get install radiotray
-
 wget http://sourceforge.net/projects/radiotray/files/releases/radiotray_0.7.3_all.deb/download
 
+#install eclipse
+#download
+cd /tmp/
+wget http://mirror.netcologne.de/eclipse/technology/epp/downloads/release/luna/R/eclipse-jee-luna-R-linux-gtk-x86_64.tar.gz
+tar -zxvf eclipse-jee-luna-R-linux-gtk-x86_64.tar.gz
+sudo chown -R root:root eclipse/
+sudo mv eclipse /opt/
+sudo ln -s /opt/eclipse/eclipse /usr/local/bin/eclipse
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+cd /tmp/
+#create eclipse starter
+cat <<EOF> /tmp/eclipse.desktop
+[Desktop Entry]
+Encoding=UTF-8
+Name=Eclipse
+Comment=Eclipse IDE
+Exec=eclipse
+Icon=/opt/eclipse/icon.xpm
+Terminal=false
+Type=Application
+Categories=Application;Development;
+StartupNotify=true
+EOF
+sudo mv /tmp/eclipse.desktop /usr/share/applications/eclipse.desktop
+sudo chmod +r /usr/share/applications/eclipse.desktop
