acd27bdb76c9fd7be23ad1b0be856cf9219ad122
Andrzej Baba <andrzej.gda@gmail.com>
2015-02-14 13:51:43 +0000

    simple bug fix

--- installiX.sh
+++ installiX.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
+
+
+choice=$(zenity \
+    --list \
+    --checklist \
+    --column "Tick box" \
+    --column "Program" \
+    --height 600 \
+    --width 500 \
+    FALSE Chromium \
+    FALSE Install-Flash-Player \
+    FALSE Deepin-Terminal \
+    FALSE Ubuntu-restricted-extras \
+    FALSE Dropbox \
+    FALSE Hardinfo \
+    FALSE Eclipse \
+    FALSE Oracle-JDK-8 \
+    FALSE Unity-Tweak-Tool \
+    FALSE Sublime-Text-3 \
+    FALSE Disable-spyware-in-ubuntu \
+    FALSE Disable-overlay-scroll-bars \
+    FALSE Shutdown-Restart-options-from-Unity-Launcher \
+    FALSE GUFW-firewall \
+    FALSE Compression-and-Decompression-tools-RAR \
+    FALSE Britghness-indicator-in-the-panel \
+    FALSE Classic-Menu-Indicator-in-the-panel \
+    )
+
+
+
+
+<<'COMMENT1'
 #install chromium
 sudo apt-get install chromium-browser
 
@@ -92,12 +124,29 @@ sudo chown -R root:root eclipse/
 sudo mv eclipse /opt/
 sudo ln -s /opt/eclipse/eclipse /usr/local/bin/eclipse
 
+#nautilus extensions
+
+#open terminal in current folder
+sudo apt-get install nautilus-open-terminal 
+
+#image extensions
+sudo add-apt-repository ppa:atareao/nautilus-extensions
+sudo apt-get update
+sudo apt-get install nautilus-document-converter nautilus-image-tools
+
+#open folder as a administrator
+sudo apt-get install nautilus-gksu
+
+
+
 cd /tmp/
 #create eclipse starter
 cat <<EOF> /tmp/eclipse.desktop
 [Desktop Entry]
 Encoding=UTF-8
 Name=Eclipse
+sudo apt-get update
+sudo apt-get install nautilus-document-converter nautilus-image-tools
 Comment=Eclipse IDE
 Exec=eclipse
 Icon=/opt/eclipse/icon.xpm
@@ -106,5 +155,6 @@ Type=Application
 Categories=Application;Development;
 StartupNotify=true
 EOF
-sudo mv /tmp/eclipse.desktop /usr/share/applications/eclipse.desktop
+sudo mv /tmp/eclipse.dgroupesktop /usr/share/applications/eclipse.desktop
 sudo chmod +r /usr/share/applications/eclipse.desktop
+COMMENT1
