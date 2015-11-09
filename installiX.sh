1bc4fcf532ab940efa87271b00bace90806a436e
Andrzej <andrzej.gda@gmail.com>
2015-11-09 01:14:43 +0000

    added spotify

--- installiX.sh
+++ installiX.sh
@@ -21,6 +21,17 @@ echo '----------------Installing codecs-----------------------'
 echo '--------------------------------------------------------'
 sudo apt-get install ubuntu-restricted-extras -y
 
+#spotify
+echo '--------------------------------------------------------'
+echo '----------------Installing spotify-----------------------'
+echo '--------------------------------------------------------'
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install spotify-client
+
+
+
 #dropbox
 echo '--------------------------------------------------------'
 echo '----------------Installing dropbox----------------------'
